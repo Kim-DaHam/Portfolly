1a3fdd88a5d0ab490f3d21cdb961e5a993fd48ea
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-12-13 23:26:20 +0900

    fix: import 경로 오타를 수정한다.
    GlobalStyle.ts 파일이 src/styles로 이동함에 따라 import 경로를 변경했습니다.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from '@/App.tsx'
-import GlobalStyle from '@/assets/styles/GlobalStyle'
+import GlobalStyle from '@/styles/GlobalStyle'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
