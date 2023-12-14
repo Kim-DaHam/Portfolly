a4f7c27bd5e5e1107a8eef9faef816c8dd1aa9e2
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-12-15 00:45:55 +0900

    feat: PortfolioItem에 캐러셀을 만든다.
    PortfolioItem 컴포넌트에 캐러셀을 추가했습니다.

--- src/components/portfolio-item/PortfolioItem.styled.tsx
+++ src/components/portfolio-item/PortfolioItem.styled.tsx
@@ -1,24 +1,89 @@
-import { styled } from 'styled-components';
+import { styled, css } from 'styled-components';
 
 import { portfolioItemSize as sizes } from '@/styles/token';
 import { Section } from '@/types/portfolio';
 
 export const PortfolioItemContainer = styled.div<{type: Section}>`
-    width: 100%;
-    aspect-ratio: ${(props) => sizes[props.type].aspectRatio};
+	width: 100%;
+	aspect-ratio: ${(props) => sizes[props.type].aspectRatio};
 
-    padding: 1.4em 1.5em 1.4em 1.5em;
+	position: relative;
+	padding: 1.4em 1.5em 1.4em 1.5em;
 
-    background-color: lightgray;
-    border-radius: 1.8rem;
-    overflow: auto;
+	background-color: lightgray;
+	border-radius: 1.8rem;
+	overflow: auto;
+	overflow: hidden;
 `
 
-export const Thumnail = styled.div`
+export const SliderWrapper = styled.div`
+	width: 100%;
+	height: 100%;
+
+
+	& .slick-initialized {
+		width: 100%;
+		height: 100%;
+		border-radius: 1.8rem;
+		background-color: gray;
+	}
+
+	& .slick-list {
+		width: 100%;
+		height: 100%;
+	}
+
+	& .slick-track {
+		height: 100%;
+	}
+
+	& .slick-dots > li {
+		margin: 0;
+		cursor: default;
+	}
+
+	& .slick-dots button {
+		pointer-events: none;
+	}
+`;
+
+export const SliderItem = styled.div`
     width: 100%;
     height: 100%;
     border-radius: 1.8rem;
 
     overflow: hidden;
     background-color: gray;
+`;
+
+const Arrow = css`
+	width: 1rem;
+	height: 1rem;
+	position: relative;
+	z-index: 100;
+`;
+
+export const ArrowWrapper = styled.div`
+	width: 100%;
+	padding: 0 0.5rem 0 0.5rem;
+
+	position: absolute;
+	z-index: 100;
+	top: 0;
+	right: 0;
+
+	display: flex;
+	justify-content: space-between;
+	top: 50%;
+`;
+
+export const PrevArrow = styled.button<{current: number}>`
+	${Arrow}
+	background-color:red;
+	visibility: ${(props) => props.current === 0 ? 'hidden' : 'visible'}
+`;
+
+export const NextArrow = styled.button<{current: number, last:number}>`
+	${Arrow}
+	visibility: ${(props) => props.current === props.last ? 'hidden' : 'visible'}
 `;
--- src/components/portfolio-item/PortfolioItem.tsx
+++ src/components/portfolio-item/PortfolioItem.tsx
@@ -1,16 +1,79 @@
-import { PortfolioItemContainer, Thumnail } from "@/components/portfolio-item/PortfolioItem.styled";
+import { useState } from "react";
+import Slider, { Settings } from "react-slick";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import { ArrowWrapper, NextArrow, PortfolioItemContainer, PrevArrow, SliderWrapper } from "@/components/portfolio-item/PortfolioItem.styled";
 import { Section } from "@/types/portfolio";
 
 function PortfolioItem(props: {type: Section}){
-    const type = props.type;
+	const type = props.type;
 
-    return(
-        <PortfolioItemContainer type={type}>
-            <Thumnail>
+	const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+	const [beforeClicked, setBeforeClicked] = useState(false);
+	const [slick, setSlick] = useState<Slider>();
 
-            </Thumnail>
-        </PortfolioItemContainer>
-    )
+	const handlePrev = ()=> {
+		if(beforeClicked) return;
+		setBeforeClicked((prev)=>!prev);
+		slick?.slickPrev();
+		setCurrentSlideIndex((prev) => prev - 1);
+		setTimeout(()=>setBeforeClicked((prev)=>!prev), 200);
+	};
+
+  const handleNext = ()=> {
+		if(beforeClicked) return;
+		setBeforeClicked((prev)=>!prev);
+		slick?.slickNext();
+		setCurrentSlideIndex((prev) => prev + 1);
+		setTimeout(()=>setBeforeClicked((prev)=>!prev), 200);
+	};
+
+	const settings: Settings = {
+		dots: true,
+		infinite: false,
+		speed: 200,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		draggable: false,
+		fade: false,
+		arrows: false,
+		vertical: false,
+		initialSlide: 0,
+		responsive: [
+			{
+				breakpoint: 960, // 화면 사이즈 960px일 때
+				settings: {
+					dots: false,
+					arrows: false,
+				}
+			}
+		]
+	}
+
+	return(
+			<PortfolioItemContainer type={type}>
+				<SliderWrapper>
+					<Slider {...settings} ref={(element)=>{
+						if(element !== null){
+							setSlick(element);
+						}
+					}}>
+						<div>1</div>
+						<div>2</div>
+						<div>3</div>
+						{/* <SliderItem>
+
+						</SliderItem> */}
+					</Slider>
+				</SliderWrapper>
+
+				<ArrowWrapper>
+					<PrevArrow onClick={handlePrev} current={currentSlideIndex}/>
+					<NextArrow onClick={handleNext} current={currentSlideIndex} last={2}/>
+				</ArrowWrapper>
+			</PortfolioItemContainer>
+	)
 }
 
 export default PortfolioItem;
