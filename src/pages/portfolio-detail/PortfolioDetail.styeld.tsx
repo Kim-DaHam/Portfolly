d4c1b7236cb7b57819accbe7238fb1bfe84b30c6
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-12-18 13:44:04 +0900

    Design: PortfolioDetail 페이지 레이아웃 영역을 구분한다.
    `background-color`로 간단히 구역을 나누었습니다.

---
+++ src/pages/portfolio-detail/PortfolioDetail.styeld.tsx
@@ -0,0 +1,107 @@
+import { styled } from 'styled-components';
+
+export const PortfolioDetailLayout = styled.div`
+	width: 100%;
+	height: 100%;
+
+	display: grid;
+	grid-template-columns: 3.5fr 1fr;
+
+	position: absolute;
+	z-index: 100;
+	top: 3.5rem;
+
+	padding: 2.5rem 3rem 2rem 3rem;
+
+	background-color: salmon;
+`;
+
+export const PortfolioContentSection = styled.section`
+	width: 100%;
+	height: 100%;
+
+	padding: 2.5rem 3rem 2rem 3rem;
+
+	background-color: lightgreen;
+`;
+
+export const HtmlContentBox = styled.div`
+	width: 100%;
+	height: 100%;
+
+	background-color: white;
+`;
+
+export const RightSideSection = styled.aside`
+	width: 100%;
+	height: 100%;
+
+	display: flex;
+	flex-direction: column;
+	gap: 2rem;
+
+	padding: 2.5rem 1rem 2rem 1rem;
+
+	background-color: cornsilk;
+`;
+
+export const UserBox = styled.div`
+	width: 100%;
+	height: 30rem;
+
+	display: flex;
+	gap: 5rem;
+
+	border-radius: 1.5rem;
+
+	background-color: violet;
+`;
+
+export const InformationBox = styled.div`
+	width: 100%;
+	height: 100%;
+
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+
+	background-color: gainsboro;
+`;
+
+export const TitleBox = styled.div`
+	width: 100%;
+	height: 5rem;
+
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+
+	background-color: white;
+`;
+
+export const TagBox = styled.div`
+	width: 100%;
+	height: 100%;
+
+	display: flex;
+
+	background-color: lightsalmon;
+`;
+
+export const SummaryBox = styled.div`
+	width: 100%;
+	height: 100%;
+
+	background-color: lightpink;
+`;
+
+export const GridBox = styled.div`
+	width: 100%;
+	height: 100%;
+
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr;
+
+	background-color: bisque;
+`;
+
+
--- src/pages/portfolio-detail/PortfolioDetail.tsx
+++ src/pages/portfolio-detail/PortfolioDetail.tsx
@@ -1,9 +1,38 @@
-function Portfolio(){
-    return(
-        <>
-        <h1>Portfolio</h1>
-        </>
-    )
+import { GridBox, HtmlContentBox, InformationBox, PortfolioContentSection, PortfolioDetailLayout, RightSideSection, SummaryBox, TagBox, TitleBox, UserBox } from "./PortfolioDetail.styeld";
+
+import Header from "@/components/header/Header";
+
+function PortfolioDetail(){
+	return(
+		<PortfolioDetailLayout>
+			<Header/>
+			<PortfolioContentSection>
+				<HtmlContentBox>
+					HtmlContentBox
+				</HtmlContentBox>
+			</PortfolioContentSection>
+
+			<RightSideSection>
+				<UserBox>
+					UserBox
+				</UserBox>
+				<InformationBox>
+					<TitleBox>
+						TitleBox
+					</TitleBox>
+					<TagBox>
+						TagBox
+					</TagBox>
+					<SummaryBox>
+						SummaryBox
+					</SummaryBox>
+				</InformationBox>
+				<GridBox>
+					GridBox
+				</GridBox>
+			</RightSideSection>
+		</PortfolioDetailLayout>
+	)
 }
 
-export default Portfolio;
+export default PortfolioDetail;
