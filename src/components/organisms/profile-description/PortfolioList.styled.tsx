a4483359078e4df36112d764b0e0edb4752bdc23
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-12-19 23:23:55 +0900

    Design: PortfolioList.tsx 컴포넌트 레이아웃을 그린다.

---
+++ src/components/organisms/profile-description/PortfolioList.styled.tsx
@@ -0,0 +1,62 @@
+import { styled } from 'styled-components';
+
+export const PortfolioListLayout = styled.div`
+	width: 100%;
+	height: 100%;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: 3rem;
+
+	background-color: lightpink;
+`;
+
+export const GridBox = styled.div`
+	width: 100%;
+	height: 100%;
+
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	gap: 1.5rem;
+`;
+
+export const GridItem = styled.div`
+	width: 100%;
+	height: 100%;
+
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+
+	cursor: pointer;
+
+	background-color: red;
+`;
+
+export const ThumbnailImage = styled.div`
+	width: 100%;
+	aspect-ratio: 1 / 1;
+
+	background-color: white;
+`;
+
+export const TitleBox = styled.div`
+	width: 100%;
+
+	display: flex;
+	flex-direction: column;
+
+	background-color: lightgreen;
+`;
+
+export const Title = styled.span`
+	font-size: 1.2rem;
+	font-weight: 600;
+`;
+
+export const Span = styled.span`
+	color: gray;
+	font-size: 1rem;
+`;
