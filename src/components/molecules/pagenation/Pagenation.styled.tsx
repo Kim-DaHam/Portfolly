b5655e3f47836e213b5d3db0d05a4d3ee9fac7a7
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-12-19 23:23:17 +0900

    Fix: Pagenation 스타일드 컴포넌트를 분리한다.
    PortfolioList.tsx에 있던 Pagenation 컴포넌트를 분리하여 재사용합니다.

---
+++ src/components/molecules/pagenation/Pagenation.styled.tsx
@@ -0,0 +1,12 @@
+import { styled } from "styled-components";
+
+export const PagenationLayout = styled.div`
+	width: 380px;
+	height: 40px;
+
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	background-color: green;
+`;
