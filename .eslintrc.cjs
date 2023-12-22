031317849b9ece633e1be0b7261232abc40af0de
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-12-22 22:34:06 +0900

    Fix: typescript관련 ESLint 규칙을 수정한다.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+		"@typescript-eslint/no-unused-vars": "off",
+		"@typescript-eslint/no-explicit-any": "off",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
