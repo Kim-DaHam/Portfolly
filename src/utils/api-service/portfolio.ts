a3018833077c730acae44ca359872eab478985d6
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-12-23 21:18:42 +0900

    Feat: portfolio 데이터를 요청하는 api 함수를 작성한다.
    전체 조회, top 포트폴리오 조회, 특정 id 조회 세 가지가 있습니다.

--- src/utils/api-service/portfolio.ts
+++ src/utils/api-service/portfolio.ts
@@ -1,8 +1,36 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { Section } from '@/types/portfolio';
 import { fetch } from '@/utils/fetch';
 
-export const fetchPortfolioList = ()=> {
-	return fetch({
-		api: '/portfolio',
-		method: 'GET',
+const portfolioKeys = {
+  all: ['portfolios'] as const,
+  lists: () => [...portfolioKeys.all, 'list'] as const,
+  list: (filters: string) => [...portfolioKeys.lists(), { filters }] as const,
+  details: () => [...portfolioKeys.all, 'detail'] as const,
+  detail: (id: string) => [...portfolioKeys.details(), id] as const,
+}
+
+export const usePortfoliosQuery = (limit: number, section: Section)=> {
+	const getPortfolios = ()=> fetch(`/portfolios?limit=${limit}&section=${section}`, 'GET');
+	return useQuery({
+		queryKey: portfolioKeys.all,
+		queryFn: getPortfolios
 	});
+};
+
+export const useTopPortfoliosQuery = ()=> {
+	const getTopPortfolios = ()=> fetch('/top-portfolios', 'GET');
+	return useQuery({
+		queryKey: portfolioKeys.list('top'),
+		queryFn: getTopPortfolios,
+	})
+};
+
+export const usePortfolioDetailQuery = (id: string)=> {
+	const getPortfolio = ()=> fetch(`/portfolios/${id}`, 'GET');
+	return useQuery({
+		queryKey: portfolioKeys.detail(id),
+		queryFn: getPortfolio,
+	})
 };
