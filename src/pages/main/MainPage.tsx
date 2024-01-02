e95ebf07248ae95e4c831a9d0bc055d67934f28b
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2024-01-02 21:28:42 +0900

    Feat: MainPage에 무한스크롤을 추가한다.

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
 import { GridBox, GridItem, MainContainer, MainLayout, PortfolioSection, Summary, Title, TitleSection } from "./MainPage.styled";
@@ -8,24 +8,38 @@ import PortfolioProfile from "@/components/molecules/profile/portfolio-profile/P
 import CategorySlider from "@/components/organisms/category-slider/CategorySlider";
 import Header from "@/components/organisms/header/Header";
 import PortfolioItem from "@/components/organisms/portfolio-item/PortfolioItem";
+import useIntersectionObserver from "@/hooks/useIntersectionObserver";
 import { section } from "@/redux/sectionSlice";
 import { Portfolio } from "@/types/portfolio";
 import { usePortfoliosQuery } from "@/utils/api-service/portfolio";
 
+const LOADED_DATA_COUNT = 10;
+const LIMIT = 100;
+
 function MainPage(){
 	const [category, setCategory] = useState<string>('전체');
+	const [lastPage, setLastPage] = useState(LOADED_DATA_COUNT);
+	const [loadNextPage, setLoadNextPage] = useState(true);
 
 	const currentSection = useSelector(section);
 
-	const { data } = usePortfoliosQuery(30, currentSection, { filterKey: 'category', filterValue: category});
+	const { data } = usePortfoliosQuery(LIMIT, currentSection, { filterKey: 'category', filterValue: category});
 	const portfolios = data;
 
 	console.log(portfolios)
 
+	const loadNewPortfolios = ()=> {
+		if(lastPage === LIMIT) {
+			setLoadNextPage(false);
+		}
+		setLastPage(prev=>prev + LOADED_DATA_COUNT);
+	}
+
+	const setObservationTarget = useIntersectionObserver(loadNewPortfolios);
+
 	return(
 		<MainLayout>
 			<Header/>
-
 			<MainContainer>
 				<TitleSection>
 					<Title>{currentSection}</Title>
@@ -36,17 +50,21 @@ function MainPage(){
 
 				<PortfolioSection>
 					<GridBox>
-						{ portfolios && portfolios.map((portfolio: Portfolio)=>{
-							return(
-								<GridItem key={portfolio.id}>
-									<PortfolioItem type={currentSection} portfolio={portfolio}/>
-									<PortfolioProfile portfolio={portfolio}/>
-								</GridItem>
-							)
-						})}
+						{ portfolios && portfolios.map((portfolio: Portfolio, index: number)=>{
+							if(index < lastPage) {
+								return(
+									<GridItem key={portfolio.id}>
+										<PortfolioItem type={currentSection} portfolio={portfolio}/>
+										<PortfolioProfile portfolio={portfolio}/>
+									</GridItem>
+								)
+							}})
+						}
+						{ loadNextPage &&
+							<div ref={setObservationTarget}></div>
+						}
 					</GridBox>
 				</PortfolioSection>
-
 			</MainContainer>
 		</MainLayout>
 	)
