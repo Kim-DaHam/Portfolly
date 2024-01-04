9e36f2a7d224d113b0bf4d305556bf04b92c262f
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2024-01-05 00:11:38 +0900

    Fix: PortfolioItem 컴포넌트 props명을 type -> section으로 바꾼다.

--- src/components/organisms/portfolio-item/PortfolioItem.tsx
+++ src/components/organisms/portfolio-item/PortfolioItem.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Slider from "react-slick";
 import 'slick-carousel/slick/slick.css';
@@ -8,47 +9,45 @@ import { initialProps, sliderSettings } from "./PortfolioItem.constants";
 
 import Image from "@/components/atoms/image/Image";
 import { ArrowBox, NextArrow, PortfolioItemLayout, PrevArrow, SliderContainer, SliderItem } from "@/components/organisms/portfolio-item/PortfolioItem.styled";
-import useHandleSlider from "@/hooks/useHandleSlider";
+import useHandleSlider from "@/hooks/slider/useHandleSlider";
+import { section as sectionSlice } from "@/redux/sectionSlice";
 import { Portfolio, Section } from "@/types/portfolio";
 import { eventStopPropagation } from "@/utils/event";
 
 
+
+
 export type Props = {
-	type: Section;
+	section?: Section;
 	portfolio: Portfolio;
 }
 
-function PortfolioItem({type, portfolio}: Props){
+function PortfolioItem({section, portfolio}: Props){
 	const navigate = useNavigate();
 	const sliderRef = useRef(null);
-	const { handlePrev, handleNext, setSlick: setSlider, currentSlideIndex } = useHandleSlider(initialProps);
-	const lastSliderIndex = portfolio?.thumbnailUrl.length - 1;
+	const lastIndex = portfolio.thumbnailUrl.length - 1;
+	const { handlePrev, handleNext, setSlider, showPrevArrow, showNextArrow } = useHandleSlider({...initialProps, lastIndex: lastIndex});
+
+	const currentSection = useSelector(sectionSlice);
 
 	useEffect(()=>{
 		setSlider(sliderRef.current!);
 	}, [])
 
 	return(
-		<PortfolioItemLayout type={type} onClick={()=>navigate(`/portfolios/${portfolio.id}`)}>
+		<PortfolioItemLayout $section={section || currentSection} onClick={()=>navigate(`/portfolios/${portfolio.id}`)}>
 			<SliderContainer>
 				<ArrowBox onClick={eventStopPropagation}>
 					<PrevArrow
 						color='White'
-						size='Fit'
 						onClick={handlePrev}
-						$current={currentSlideIndex}
-					>
-						Prev
-					</PrevArrow>
+						$showPrevArrow={showPrevArrow}>
+					Prev</PrevArrow>
 					<NextArrow
 						color='White'
-						size='Fit'
 						onClick={handleNext}
-						$current={currentSlideIndex}
-						$last={lastSliderIndex}
-					>
-						Next
-					</NextArrow>
+						$showNextArrow={showNextArrow}>
+					Next</NextArrow>
 				</ArrowBox>
 
 				<Slider {...sliderSettings} ref={sliderRef}>
