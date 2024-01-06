3d7a410cf01a186b995d181d3573d37075f9cf8e
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2024-01-06 15:33:53 +0900

    Fix: useParams 훅을 사용해 파라미터를 가져온다.
    /:section URL 파라미터를 가져올 때 useParams를 사용합니다.

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,5 +1,6 @@
 import { Suspense, lazy, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 import { MainContainer, MainLayout, PortfolioSection, Summary, Title, TitleSection } from "./MainPage.styled";
 
@@ -7,8 +8,8 @@ import { mainPageSectionSummary } from '@/assets/data/phrase';
 import CategorySlider from "@/components/organisms/category-slider/CategorySlider";
 import Header from "@/components/organisms/header/Header";
 import PortfolioListSkeleton from "@/components/skeletons/portfolio-list/PortfolioListSkeleton";
-import { section as sectionSlice, setSection } from "@/redux/sectionSlice";
-import { getSection } from "@/utils/portfolio";
+import { section, setSection } from "@/redux/sectionSlice";
+import { sectionUrlParameterMap } from "@/utils/path";
 
 const PortfolioList = lazy(() => import('@/components/organisms/portfolio-list/PortfolioList'));
 
@@ -16,12 +17,14 @@ function MainPage(){
 	const [category, setCategory] = useState('전체');
 
 	const dispatch = useDispatch();
-	const currentSection = useSelector(sectionSlice);
+
+	const params = useParams() as {section: string};
+	const currentSection = useSelector(section);
 
 	const getSectionUrlParameter = () => {
-		const section = getSection();
+		const section = sectionUrlParameterMap[params.section];
 		dispatch(setSection(section));
-	}
+	};
 
 	useEffect(()=>{
 		window.addEventListener("popstate", getSectionUrlParameter);
