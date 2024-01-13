50c33ebbfd56ebadc60a9942e04427e0fdaec52d
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2024-01-14 01:56:28 +0900

    Feat: PortfolioDetailPage에 수정하기, 삭제하기 버튼을 추가한다.

--- src/pages/portfolio-detail/PortfolioDetailPage.tsx
+++ src/pages/portfolio-detail/PortfolioDetailPage.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from "react";
 import { FiChevronRight as ChevronRightIcon } from "react-icons/fi";
+import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 
-import { Aside, ButtonGroup, Content, FlexBox, GridBox, GridItem, HtmlContent, PortfolioInfoBox, PortfolioSection, ProfileBox, SummaryBox, TagBox, TitleBox, Wrapper } from "./PortfolioDetailPage.styled";
+import { Aside, ButtonGroup, Content, FlexBox, GridBox, GridItem, HtmlContent, PortfolioInfoBox, PortfolioSection, ProfileBox, SummaryBox, TagBox, TextButton, TitleBox, Wrapper } from "./PortfolioDetailPage.styled";
 
 import { RoundButton, SquareButton } from "@/components/atoms/button/Button.styled";
 import ToggleButton from "@/components/atoms/button/ToggleButton";
@@ -9,15 +11,25 @@ import Image from "@/components/atoms/image/Image";
 import Tag from "@/components/atoms/tag/Tag";
 import Profile from "@/components/molecules/profile/Profile";
 import Header from "@/components/organisms/header/Header";
+import { userId } from "@/redux/loginSlice";
 import { Heading, Label, Text } from "@/styles/Text.styled";
 import { Portfolio } from "@/types/portfolio";
 import { usePortfolioDetailQuery } from "@/utils/api-service/portfolio";
 
 export default function PortfolioDetail(){
+	const [hasAuthority, setHasAuthority] = useState(false);
+
 	const portfolioId = useParams().portfolio_id as string;
 	const { data: portfolio } = usePortfolioDetailQuery(portfolioId);
 
 	const navigate = useNavigate();
+
+	const user = useSelector(userId);
+
+	useEffect(() => {
+		const hasAuthority = (user === portfolio.user.id) ? true : false;
+		setHasAuthority(hasAuthority);
+	}, [])
 
 	return(
 		<Wrapper>
@@ -42,11 +54,19 @@ export default function PortfolioDetail(){
 								<TitleBox>
 									<Text size='Medium' color='Gray'>{portfolio.category}</Text>
 									<Heading size='Small'>{portfolio.title}</Heading>
-									<ButtonGroup>
-										<ToggleButton type='like' isToggled={portfolio.isLiked} portfolioId={portfolio.id} currentLikes={portfolio.likes}/>
-										<ToggleButton type='bookmark' isToggled={portfolio.isBookmarked} portfolioId={portfolio.id}/>
-									</ButtonGroup>
 								</TitleBox>
+
+								<ButtonGroup>
+									<ToggleButton type='like' isToggled={portfolio.isLiked} portfolioId={portfolio.id} currentLikes={portfolio.likes}/>
+									<ToggleButton type='bookmark' isToggled={portfolio.isBookmarked} portfolioId={portfolio.id}/>
+								</ButtonGroup>
+
+								{ hasAuthority &&
+									<ButtonGroup>
+										<TextButton>수정하기</TextButton>
+										<TextButton>삭제하기</TextButton>
+									</ButtonGroup>
+								}
 
 								<Label>태그</Label>
 								<TagBox>
