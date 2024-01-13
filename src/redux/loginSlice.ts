b232425d98e8a3b500dce1015a151bca30ee4dd9
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2024-01-13 23:33:09 +0900

    Fix: login 전역 상태에 userId를 추가한다.

--- src/redux/loginSlice.ts
+++ src/redux/loginSlice.ts
@@ -5,11 +5,13 @@ import { RootState } from './store';
 type InitialState = {
 	authority: 'expert' | 'client',
 	isLogin: boolean,
+	userId: number,
 }
 
 const initialState: InitialState = {
 	authority: 'expert',
 	isLogin: true,
+	userId: 100,
 };
 
 export const loginSlice = createSlice({
@@ -27,5 +29,6 @@ export const loginSlice = createSlice({
 
 export const { actions, reducer } = loginSlice;
 export const isLogin = (state: RootState) => state.auth.isLogin;
+export const userId = (state: RootState) => state.auth.userId;
 
 export default loginSlice;
