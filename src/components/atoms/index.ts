f55f4c12e6ade702862c0a58d5cbb92cb29c4556
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2024-01-15 22:06:38 +0900

    Feat: /atoms 폴더 하위 배럴 파일을 생성한다.

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,13 @@
+import ToggleButton from "@/components/atoms/button/ToggleButton";
+import Image from "@/components/atoms/image/Image";
+import Selector from "@/components/atoms/selector/Selector";
+import Tag from "@/components/atoms/tag/Tag";
+
+export * from '@/components/atoms/button/Button.styled'
+
+export {
+	ToggleButton,
+	Image,
+	Selector,
+	Tag,
+};
