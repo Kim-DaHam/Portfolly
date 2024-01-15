dc5210799fedb004b194eb3dd1229bd578f8903e
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2024-01-16 02:43:41 +0900

    Fix: UserInformation.tsx 코드를 정리한다.

--- src/components/organisms/user-information/UserInformation.tsx
+++ src/components/organisms/user-information/UserInformation.tsx
@@ -1,28 +1,26 @@
-import { UserInformationLayout } from "./UserInformation.styled";
-
-import { FlexColumnBox, FlexBox } from "@/styles/Container.styled";
+import * as S from "@/components/organisms/user-information/UserInformation.styled";
 import { Label, Text } from "@/styles/Text.styled";
 
 export default function UserInformation() {
 	return(
-		<UserInformationLayout>
+		<S.Wrapper>
 			<Label>활동 정보</Label>
-			<FlexColumnBox gap='0.5rem'>
-				<FlexBox justify='space-between'>
+			<S.Box>
+				<S.Group>
 					<Text size='Medium'>총 작업 수</Text>
 					<Text size='Medium'>0개</Text>
-				</FlexBox>
+				</S.Group>
 
-				<FlexBox justify='space-between'>
+				<S.Group>
 					<Text size='Medium'>만족도</Text>
 					<Text size='Medium'>0%</Text>
-				</FlexBox>
+				</S.Group>
 
-				<FlexBox justify='space-between'>
+				<S.Group>
 					<Text size='Medium'>연락 가능 시간</Text>
 					<Text size='Medium'>언제나 가능</Text>
-				</FlexBox>
-			</FlexColumnBox>
-		</UserInformationLayout>
+				</S.Group>
+			</S.Box>
+		</S.Wrapper>
 	)
 }
