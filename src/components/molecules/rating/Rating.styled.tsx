77c9662048187d11808619ae3c631e76689f4123
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2024-01-26 22:56:42 +0900

    Feat: Rating.tsx에 별점 부여 기능을 추가한다.

--- src/components/molecules/rating/Rating.styled.tsx
+++ src/components/molecules/rating/Rating.styled.tsx
@@ -1,20 +1,65 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Wrapper = styled.div`
-	width: fit-content;
-
 	display: flex;
-	gap: 0.4rem;
-	align-items: center;
-
-	background-color: yellow;
+  align-items: center;
+  gap: 8px;
 `;
 
 export const Content = styled.div`
-	display: flex;
-	align-items: center;
-	gap: 0.1rem;
-	flex-basis: 1;
+	position: relative;
+  display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
 
-	background-color: salmon;
+  input:checked ~ label,
+  labeL:hover,
+  labeL:hover ~ label {
+    transition: 0.2s;
+    color: orange;
+  }
+`;
+
+export const Input = styled.input`
+	display: none;
+`;
+
+export const Label = styled.label<{isHalf: boolean}>`
+	cursor: pointer;
+  font-size: 1.5rem;
+  color: lightgray;
+
+	${(props) => {
+		if(props.isHalf)
+			return css`
+				position: absolute;
+				width: 12px;
+				overflow: hidden;
+
+				&:nth-of-type(10) {
+					transform: translate(-108px);
+				}
+				&:nth-of-type(8) {
+					transform: translate(-84px);
+				}
+				&:nth-of-type(6) {
+					transform: translate(-60px);
+				}
+				&:nth-of-type(4) {
+					transform: translate(-36px);
+				}
+				&:nth-of-type(2) {
+					transform: translate(-12px);
+				}
+			`
+	}}
+`;
+
+export const StarRate = styled.div`
+	width: 5rem;
+	height: 100%;
+
+	position: absolute;
+
+	background-color: pink;
 `;
--- src/components/molecules/rating/Rating.tsx
+++ src/components/molecules/rating/Rating.tsx
@@ -1,17 +1,36 @@
-import { FiStar as StarIcon } from "react-icons/fi";
+import { Fragment, useState } from "react";
+import { FaStar as Star, FaStarHalf as HalfStar } from "react-icons/fa";
 
 import { Text } from '@/components';
 import * as S from "@/components/molecules/rating/Rating.styled";
 
-export default function Rating() {
+type Props = {
+	readonly?: boolean;
+	setValue?: any;
+	score?: number;
+};
+
+export default function Rating({ readonly, setValue, score=0 }: Props) {
+	const [rating, setRating] = useState(score);
+
 	return(
 		<S.Wrapper>
 			<S.Content>
-				{new Array(0).fill(0, 0, 5).map((_, index: number) => {
-					return <StarIcon key={index}/>;
+				{new Array(10).fill(0).map((_, index: number) => {
+					const value = 5 - index * 0.5;
+					const isHalf = index % 2 === 0 || index === 0 ? false : true;
+
+					return (
+						<Fragment key={index}>
+							<S.Input type='radio' id={`star${value}`} value={value}/>
+							<S.Label onClick={() => setRating(value)} isHalf={isHalf} htmlFor={`stars${value}`}>
+								{ isHalf ? <HalfStar /> : <Star />}
+							</S.Label>
+						</Fragment>
+					);
 				})}
 			</S.Content>
-			<Text type='common' color='gray'>Score</Text>
+			<Text type='common' color='gray'>{rating} / 5</Text>
 		</S.Wrapper>
 	)
 }
