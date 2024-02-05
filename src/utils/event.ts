0f3aa467ffc940d8555be67aa4f21f940094acd8
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2024-02-05 20:25:00 +0900

    Remove: useStopScrollY.ts 훅을 제거하고 utils 함수로 이동한다.

--- src/utils/event.ts
+++ src/utils/event.ts
@@ -1,3 +1,19 @@
 export const eventStopPropagation = (event: React.MouseEvent)=>{
 	event.stopPropagation();
 };
+
+export const stopScrollY = () => {
+	document.body.style.cssText = `
+		position: fixed;
+		top: -${window.scrollY}px;
+		overflow-y: scroll;
+		width: 100%;
+		overflow: hidden;
+	`
+};
+
+export const moveScrollY = () => {
+	const scrollY = document.body.style.top;
+	document.body.style.cssText = '';
+	window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+};
