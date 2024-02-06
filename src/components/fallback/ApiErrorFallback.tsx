a9765e6ef7cd421902f56bc1dba177ffb29e9b45
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2024-02-06 23:11:29 +0900

    Feat: ApiErrorFallback.tsx 를 생성한다.

--- src/components/fallback/ApiErrorFallback.tsx
+++ src/components/fallback/ApiErrorFallback.tsx
@@ -1,21 +1,37 @@
-import Error from '@/components/error/Error';
+import { useNavigate } from 'react-router-dom';
 
 import type { FallbackProps } from 'react-error-boundary';
 
+import { getErrorMessage } from '@/utils';
+
+import { Error } from '@/components';
+
 function ApiErrorFallback({error, resetErrorBoundary}: FallbackProps) {
+	const navigate = useNavigate();
+
 	const { status } = error.response;
+	const message = getErrorMessage(status);
 
-	// if(status) {
-	// 	return(
-	// 		<Error reset={resetErrorBoundary}/>
-	// 	)
-	// }
+	if(status === 401 || status === 402) {
+		const errorHandler = () => {
+			navigate(`/login`);
+		};
+		return (
+			<Error
+				type='component'
+				reset={errorHandler}
+				message={message}
+			/>
+		)
+	}
 
-	// if(status) {
-	// 	return(
-	// 		<Error reset={resetErrorBoundary}/>
-	// 	)
-	// }
+	return(
+		<Error
+			type='component'
+			reset={resetErrorBoundary}
+			message={message}
+		/>
+	)
 }
 
 export default ApiErrorFallback;
