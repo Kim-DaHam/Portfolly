9c15342f960c65103428b9ed3eaa8244ef7cdc5f
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2024-02-06 12:57:33 +0900

    Design: MyPageNavigator.tsx 디자인을 적용한다.

--- src/components/molecules/navigator/mypage-navigator/MypageNavigator.tsx
+++ src/components/molecules/navigator/mypage-navigator/MypageNavigator.tsx
@@ -11,34 +11,43 @@ type Props = {
 export default function MyPageNavigator({auth, isMyPage}: Props) {
 	const params = useParams();
 	const profileId = params.id as string;
+	const tab = params.tab as string;
 
 	return (
 		<S.Wrapper>
-			<S.Tab>
-				<Link to={`/profile/${profileId}`}>소개</Link>
+			<S.Tab $active={!tab}>
+				<Link to={`/profile/${profileId}`}>
+					소개
+				</Link>
 			</S.Tab>
 
 			{ auth === 'expert' &&
 				<>
-					<S.Tab>
-						<Link to={`/profile/${profileId}/portfolios?page=1`}>포트폴리오</Link>
-					</S.Tab>
-					<S.Tab>
-						<Link to={`/profile/${profileId}/reviews?page=1`}>리뷰</Link>
-					</S.Tab>
+				<S.Tab $active={tab === 'portfolios'}>
+					<Link to={`/profile/${profileId}/portfolios?page=1`}>
+						포트폴리오
+					</Link>
+				</S.Tab>
+				<S.Tab $active={tab === 'reviews'}>
+					<Link to={`/profile/${profileId}/reviews?page=1`}>
+						리뷰
+					</Link>
+				</S.Tab>
 				</>
 			}
 
 			{ isMyPage &&
 				<>
-				<S.Tab>
+				<S.Tab $active={tab === 'management'}>
 					<Link to={`/profile/${profileId}/management?page=1`}>
 						{auth === 'client' ? '구매 관리' : '판매 관리'}
 					</Link>
 				</S.Tab>
 
-				<S.Tab>
-					<Link to={`/profile/${profileId}/bookmarks?page=1`}>북마크</Link>
+				<S.Tab $active={tab === 'bookmarks'}>
+					<Link to={`/profile/${profileId}/bookmarks?page=1`}>
+						북마크
+					</Link>
 				</S.Tab>
 				</>
 			}
