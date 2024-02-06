7e557d7dcde32903b2f531d3ccd0fce42ee19d35
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2024-02-06 20:45:02 +0900

    Feat: Error.tsx 컴포넌트를 생성한다.
    에러 화면을 대신 보여주는 컴포넌트입니다.

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,10 +1,32 @@
+import { FiRotateCw as RetryIcon } from "react-icons/fi";
+
+import * as S from "@/pages/error/Error.styled";
+
+import { Button, Text } from "@/components";
+
 type Props = {
 	reset: (...args: any[]) => void;
+	message: {
+		title: string,
+		content: string,
+	};
 }
 
-function Error({reset}: Props) {
+function Error({ reset, message }: Props) {
 	return(
-		<div>Error</div>
+		<S.Wrapper>
+			<Text size='headingMedium'>
+				{message.title}
+			</Text>
+			<Text size='bodyLarge' color='lightgray'>
+				{message.content}
+			</Text>
+			<RetryIcon
+				size={30}
+				color='lightgray'
+				onClick={reset}
+			/>
+		</S.Wrapper>
 	)
 }
 
