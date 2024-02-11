79bbafe3140af0d69eed0a8ca848e301209e5399
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2024-02-11 23:20:15 +0900

    Remove: SearchModal.constants.ts를 삭제한다.

--- src/components/organisms/modal/search-modal/index.ts
+++
@@ -1,3 +0,0 @@
-export * from '@/components/organisms/modal/search-modal/SearchModal.constants';
-export * from '@/components/organisms/modal/search-modal/SearchModal.helpers';
-export * from '@/components/organisms/modal/search-modal/SearchModal';
